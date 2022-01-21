c5e0efbf3ab185c31c55e87f8ccd05bd5cdb6b23
Thomas Vargas <tavargas2012irc@gmail.com>
2022-01-21 16:04:04 -0600

    mostly done, still needs styling + color choice + gradient + delete input on submit.

--- script.js
+++ script.js
@@ -1,29 +1,81 @@
-const container = document.getElementById("container");
-const startBtn = document.getElementById("start-btn");
+const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = '#333333';
+const DEFAULT_MODE = 'color';
 
-startBtn.addEventListener("click", function() {
-    startButton();
+let currentMode = DEFAULT_MODE;
+let currentColor = DEFAULT_COLOR;
+let currentSize = DEFAULT_SIZE;
+
+const container = document.getElementById("grid-container");
+const clearBtn = document.getElementById("clearBtn");
+const resizeBtn = document.getElementById("resize-btn");
+const eraserBtn = document.getElementById("eraser-btn");
+const paintBtn = document.getElementById("paint-btn");
+const input = document.getElementById("new-size");
+
+paintBtn.addEventListener("click", function() {
+    currentMode = 'color';
 });
 
-function startButton () {
+clearBtn.addEventListener("click", function() {
     clearGrid();
+    createGrid(currentSize);
+});
 
-    let userInput = prompt("What size sides would you like for your grid?");
-    createGrid(userInput, userInput);
-};
+resizeBtn.addEventListener("click", function() {
+    const newSize = document.getElementById("new-size").value;
+    resizeGrid(newSize);
+});
 
-function createGrid(rows, cols) {
-    container.style.setProperty("--grid-rows", rows);
-    container.style.setProperty("--grid-cols", cols);
-    for(let i = 0; i < (rows * cols); i++) {
-        let box = document.createElement("div");
-        //box.innerText = (i + 1);
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        document.getElementById("resize-btn").click();
+    }
+});
+
+eraserBtn.addEventListener("click", function() {
+    currentMode = "eraser";
+});
+
+function handleChange(input) {
+    if (input.value < 0) input.value = 0;
+    if (input.value > 100) input.value = 100;
+}
+
+function setCurrentMode(newMode) {
+    activateButton(newMode);
+    currentMode = newMode;
+}
+
+function createGrid(size) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for(let i = 0; i < size * size; i++) {
+        const box = document.createElement("div");
+        box.addEventListener('mouseover', changeColor);
         container.appendChild(box).className = "grid-item";
     };
+};
+
+function changeColor(e) {
+    if (currentMode === 'color') {
+      e.target.style.backgroundColor = currentColor;
+    } else if (currentMode === 'eraser') {
+      e.target.style.backgroundColor = '#fefefe';
+    }
+}
+
+function resizeGrid(newSize) {
+    currentSize = newSize;
+    clearGrid();
+    createGrid(newSize);
 };
 
 function clearGrid() {
     container.innerHTML = "";
 };
 
-createGrid(16, 16);
+window.onload = () => {
+    createGrid(DEFAULT_SIZE);
+};
